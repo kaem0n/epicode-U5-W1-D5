ea529fd2739b99af89c19c72430daf0b85e9cbd8
Antonio Ruggia Piquer <149959739+kaem0n@users.noreply.github.com>
2024-04-07 20:25:11 +0200

    added count based check to prevent creating too many same type stations on the same building

--- src/main/java/kaem0n/u5w1d5/dao/StationDAO.java
+++ src/main/java/kaem0n/u5w1d5/dao/StationDAO.java
@@ -2,10 +2,14 @@ package kaem0n.u5w1d5.dao;
 
 import kaem0n.u5w1d5.entities.Building;
 import kaem0n.u5w1d5.entities.Station;
+import kaem0n.u5w1d5.entities.StationType;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface StationDAO extends JpaRepository<Station, Long> {
+
     boolean existsByDescriptionAndBuilding(String description, Building building);
+
+    long countByTypeAndBuilding(StationType type, Building building);
 }
--- src/main/java/kaem0n/u5w1d5/dao/StationService.java
+++ src/main/java/kaem0n/u5w1d5/dao/StationService.java
@@ -11,7 +11,8 @@ public class StationService {
     private StationDAO sd;
 
     public void save(Station station) {
-        if (!sd.existsByDescriptionAndBuilding(station.getDescription(), station.getBuilding())) {
+        long sameTypeStations = sd.countByTypeAndBuilding(station.getType(), station.getBuilding());
+        if (!sd.existsByDescriptionAndBuilding(station.getDescription(), station.getBuilding()) && sameTypeStations < 3) {
             sd.save(station);
             System.out.println("New " + station.getType().toString().toLowerCase() + " station in " + station.getBuilding().getName() +
                     " (" + station.getBuilding().getCity() + ") building registered on db.");
